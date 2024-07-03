e4cbe535988d9c6d7a4b754694eee1f21f73ffb5
ian <ian@brandnewbox.com>
2024-07-03 19:15:05 +0100

    leveraging stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,12 +1,4 @@
 import { application } from "./application"
 
-// import HelloController from "./hello_controller"
-// application.register("hello", HelloController)
-
 import ToggleController from "./toggle_controller"
 application.register("toggle", ToggleController)
-//TODO IAN for this we can leverage stimulus classes and define the classes we want to toggle WHERE we use the controller
-
-// TRY to do the 2nd one using the same controller and if it doesnt' work
-// then create a page controller that has targets to the two sets of html and clicking the button should 
-// change the page
--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,9 +1,37 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  // static classes = [ "loading" ] 
 
-  connect() {
-    console.log("Hello, Stimulus!", this.element)
+  static classes = [ "show", "hide", "open" ] 
+  static targets = [ "menuIcon", "closeBtn", "sidebar", "primaryOptions", "secondaryOptions" ]
+  
+  open() {
+    this.menuIconTarget.classList.add(this.hideClass)
+    this.closeBtnTarget.classList.remove(this.hideClass)
+    this.closeBtnTarget.classList.add(this.showClasses[0])
+    this.sidebarTarget.classList.add(this.openClass)
+  }
+  
+  close(){
+    this.closeBtnTarget.classList.add(this.hideClass)
+    this.menuIconTarget.classList.add(this.showClasses[0])
+    this.menuIconTarget.classList.remove(this.hideClass)
+    this.sidebarTarget.classList.remove(this.openClass)
+  }
+
+  newEntry(){
+    this.sidebarTarget.classList.add('align-start')
+    this.primaryOptionsTarget.classList.remove(this.showClasses[0])
+    this.primaryOptionsTarget.classList.add(this.hideClass)
+    this.secondaryOptionsTarget.classList.add(this.showClasses[1])
+    this.secondaryOptionsTarget.classList.remove(this.hideClass)
+  }
+
+  closeNewEntry(){
+    this.sidebarTarget.classList.remove('align-start')
+    this.primaryOptionsTarget.classList.add(this.showClasses[1])
+    this.primaryOptionsTarget.classList.remove(this.hideClass)
+    this.secondaryOptionsTarget.classList.remove(this.showClasses[1])
+    this.secondaryOptionsTarget.classList.add(this.hideClass)
   }
 }
